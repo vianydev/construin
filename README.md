b933c845ab0e2526ef557be7200ea6b47acc0865
Vianey Marcelo <vianymoz@gmail.com>
2023-05-16 22:50:14 -0600

    start project

---
+++ README.md
@@ -0,0 +1,75 @@
+# Project Name
+
+This is a projects. 
+
+![Design preview for the project](./design/desktop-preview.jpg)
+
+| [![](screenshots/homepage-.png)](https://) | [![](screenshots/homepage-.png)](https://) | [![](screenshots/about.png)](https://) |
+|:---:|:---:|:---:|
+| **Homepage 1**  | **Homepage 2**  | **About**  |
+| [![](screenshots/servie.png)](https:///service.html) | [![](screenshots/projec.png)](https://) | [![](screenshots/.png)](https://) |
+| **Service** | **About** | **Contact** |
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
+
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+
+Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
+
+### Links
+
+- Solution URL: [Solution URL](https://your-solution-url.com)
+- Live Site URL: [Live site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### Resources
+
+* **Bootstrap v5.3.0-alpha3**: <https://getbootstrap.com/docs/5.3/getting-started/download/>
+* **Jquery v3.5.1**: <https://jquery.com/download/>
+* **Google Fonts**: <http://fonts.google.com/>
+* **Font Awesome Free**: <https://fontawesome.com/>
+* **Animate CSS**: <https://animate.style/>
+* **Colorbox**: <https://www.jacklmoore.com/colorbox/>
+* **Slick**: <https://kenwheeler.github.io/slick/>
+
+## Author
+
+Designed & Developed by [vianydev](https://vianydev.github.io)
+- Twitter - [@vianydev](https://www.twitter.com/vianydev)
+- Linkedin - [@vianydev](https://www.linkedin.com/in/vianydev/)
+- Github - [@vianydev](https://github.com/vianydev)
+
+
---
+++ js/script.js
@@ -0,0 +1,175 @@
+jQuery(function ($) {
+	'use strict';
+
+	/* ----------------------------------------------------------- */
+	/*  Fixed header
+	/* ----------------------------------------------------------- */
+	$(window).on('scroll', function () {
+
+		// fixedHeader on scroll
+		function fixedHeader() {
+			var headerTopBar = $('.top-bar').outerHeight();
+			var headerOneTopSpace = $('.header-one .logo-area').outerHeight();
+
+			var headerOneELement = $('.header-one .site-navigation');
+			var headerTwoELement = $('.header-two .site-navigation');
+
+			if ($(window).scrollTop() > headerTopBar + headerOneTopSpace) {
+				$(headerOneELement).addClass('navbar-fixed');
+				$('.header-one').css('margin-bottom', headerOneELement.outerHeight());
+			} else {
+				$(headerOneELement).removeClass('navbar-fixed');
+				$('.header-one').css('margin-bottom', 0);
+			}
+			if ($(window).scrollTop() > headerTopBar) {
+				$(headerTwoELement).addClass('navbar-fixed');
+				$('.header-two').css('margin-bottom', headerTwoELement.outerHeight());
+			} else {
+				$(headerTwoELement).removeClass('navbar-fixed');
+				$('.header-two').css('margin-bottom', 0);
+			}
+		}
+		fixedHeader();
+
+		// Count Up
+		function counter() {
+			var oTop;
+			if ($('.counterUp').length !== 0) {
+				oTop = $('.counterUp').offset().top - window.innerHeight;
+			}
+			if ($(window).scrollTop() > oTop) {
+				$('.counterUp').each(function () {
+					var $this = $(this),
+						countTo = $this.attr('data-count');
+					$({
+						countNum: $this.text()
+					}).animate({
+						countNum: countTo
+					}, {
+						duration: 1000,
+						easing: 'swing',
+						step: function () {
+							$this.text(Math.floor(this.countNum));
+						},
+						complete: function () {
+							$this.text(this.countNum);
+						}
+					});
+				});
+			}
+		}
+		counter();
+
+		// scroll to top btn show/hide
+		function scrollTopBtn() {
+			var scrollToTop = $('#back-to-top'),
+				scroll = $(window).scrollTop();
+			if (scroll >= 50) {
+				scrollToTop.fadeIn();
+			} else {
+				scrollToTop.fadeOut();
+			}
+		}
+		scrollTopBtn();
+	});
+
+
+	$(document).ready(function () {
+
+		// back to top
+		function backToTop() {
+			$('#back-to-top').on('click', function () {
+				$('#back-to-top').tooltip('hide');
+				$('body,html').animate({
+					scrollTop: 0
+				}, 500);
+				return false;
+			});
+		}
+		backToTop();
+
+		// Hero-carousel
+		function bannerCarouselOne() {
+			$('.banner-carousel.banner-carousel-1').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: true,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+			$('.banner-carousel.banner-carousel-1').slickAnimation();
+		}
+		bannerCarouselOne();
+
+		// testimonial carousel
+		function testimonialCarousel() {
+			$('.testimonial-slide').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+				speed: 600,
+				arrows: false
+			});
+		}
+		testimonialCarousel();
+
+		// team carousel
+		function teamCarousel() {
+			$('.team-slide').slick({
+				dots: false,
+				infinite: false,
+				speed: 300,
+				slidesToShow: 4,
+				slidesToScroll: 2,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>',
+				responsive: [{
+						breakpoint: 992,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3
+						}
+					},
+					{
+						breakpoint: 768,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 481,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+		}
+		teamCarousel();
+
+
+		// media popup
+		function mediaPopup() {
+			$('.gallery-popup').colorbox({
+				rel: 'gallery-popup',
+				transition: 'slideshow',
+				innerHeight: '500'
+			});
+			$('.popup').colorbox({
+				iframe: true,
+				innerWidth: 600,
+				innerHeight: 400
+			});
+		}
+		mediaPopup();
+
+	});
+
+
+});
